2f042db454a4abe6b608869c50d0c27516effbd0
Ênio Filipe <eniofms@protonmail.com>
2020-11-17 10:17:58 -0300

    Revert "feature/categorias"

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from '../Pages/Login';
 import Dashboard from '../Pages/Dashboard';
-import Categorias from '../Pages/Categorias';
 
 const index = () => {
   return (
@@ -10,7 +9,6 @@ const index = () => {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/dashboard" exact component={Dashboard} />
-        <Route path="/categorias" exact component={Categorias} />
       </Switch>
     </BrowserRouter>
   )
