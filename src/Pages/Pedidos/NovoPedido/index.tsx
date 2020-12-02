0a23f5adbd0681836f385bbcac2313eb78d21dfe
Deise Santana <60986602+deisesan@users.noreply.github.com>
2020-12-02 12:22:57 -0300

    Botão Ajustado

--- src/Pages/Pedidos/NovoPedido/index.tsx
+++ src/Pages/Pedidos/NovoPedido/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
   Button,
@@ -9,14 +10,20 @@ import {
   Table,
   Paper,
   TextField,
-  ButtonGroup,
+  MenuItem,
+  Select,
   FormControl,
-  NativeSelect,
-  Grid,
-  Popper,
+  InputLabel,
 } from '@material-ui/core';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { makeStyles } from '@material-ui/core/styles';
 import { Container, AddOrderContainer } from './styles';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(2),
+    minWidth: 200,
+  },
+}));
 
 const rows = [
   {
@@ -39,55 +46,85 @@ const rows = [
   },
 ];
 
-const options = ['Dinheiro', 'Cartão de Débito'];
+const index = () => {
+  const classes = useStyles();
+  const options = ['Dinheiro', 'Cartão de Débito'];
+  const [type, setType] = React.useState('');
+  const [open, setOpen] = React.useState(false);
 
-const index = () => (
-  <Container>
-    <AddOrderContainer>
-      <span>Cliente:</span>
-      <TextField disabled id="outlined-basic" variant="outlined" />
-      <ButtonGroup variant="contained" orientation="vertical" color="primary">
-        <Button>Dinheiro</Button>
-        <Button>Cartão</Button>
-        <Button>Cheque</Button>
-        <ArrowDropDownIcon />
-      </ButtonGroup>
-      <Button>Adicionar Cliente</Button>
-      <Button>Adicionar Produto</Button>
-    </AddOrderContainer>
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Quantidade</TableCell>
-            <TableCell>Código</TableCell>
-            <TableCell>Nome</TableCell>
-            <TableCell>Descrição</TableCell>
-            <TableCell>Categorias</TableCell>
-            <TableCell>Preço</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((item) => (
-            <TableRow hover tabIndex={-1} key={`cod${item.id}`}>
-              <TableCell>{item.quantity}</TableCell>
-              <TableCell>{item.id}</TableCell>
-              <TableCell>{item.name}</TableCell>
-              <TableCell>{item.status}</TableCell>
-              <TableCell>{item.category}</TableCell>
-              <TableCell>
-                {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.price)}
-              </TableCell>
-              <TableCell>{item.delete()}</TableCell>
+  const handleChange = (event: any) => {
+    setType(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <Container>
+      <AddOrderContainer>
+        <span>Cliente:</span>
+        <TextField disabled id="outlined-basic" variant="outlined" />
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-controlled-open-select-label">Tipo de Pagamento</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            open={open}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            value={type}
+            onChange={handleChange}
+          >
+            {options &&
+              options.map((op, i) => (
+                <MenuItem value={op} key={i}>
+                  {op}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        <Button>Adicionar Cliente</Button>
+        <Button>Adicionar Produto</Button>
+      </AddOrderContainer>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Quantidade</TableCell>
+              <TableCell>Código</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>Descrição</TableCell>
+              <TableCell>Categorias</TableCell>
+              <TableCell>Preço</TableCell>
             </TableRow>
-          ))}
-          <TableRow />
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <Button>Voltar</Button>
-    <Button>Salvar</Button>
-  </Container>
-);
+          </TableHead>
+          <TableBody>
+            {rows.map((item) => (
+              <TableRow hover tabIndex={-1} key={`cod${item.id}`}>
+                <TableCell>{item.quantity}</TableCell>
+                <TableCell>{item.id}</TableCell>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.status}</TableCell>
+                <TableCell>{item.category}</TableCell>
+                <TableCell>
+                  {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.price)}
+                </TableCell>
+                <TableCell>{item.delete()}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow />
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button>Voltar</Button>
+      <Button>Salvar</Button>
+    </Container>
+  );
+};
 
 export default index;
