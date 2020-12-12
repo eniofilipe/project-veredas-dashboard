06317489cace1b3d8f50fa2b4d1b777bd8558c97
Deise Santana <60986602+deisesan@users.noreply.github.com>
2020-12-12 17:09:35 -0300

    Saindo.

--- src/Pages/Ofertas/EditarOferta/index.tsx
+++ src/Pages/Ofertas/EditarOferta/index.tsx
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Container, TableContainer, TableHead, TableBody, TableCell, TableRow } from '@material-ui/core';
+import {
+  Container,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table,
+  TableRow,
+  TextField,
+  Button,
+} from '@material-ui/core';
+import { AddProductContainer } from './styles';
 
 import { Oferta } from '../../../Types';
 import { getProdutosOfertas } from '../../../Api/Ofertas';
@@ -27,16 +38,30 @@ const index = () => {
 
   return (
     <Container>
+      <AddProductContainer>
+        Produto:
+        {/* Adicionar select do produto */}
+        <TextField id="outlined-basic" variant="outlined" />
+        Quantidade:
+        <TextField id="outlined-basic" variant="outlined" />
+        Preço:
+        <TextField id="outlined-basic" variant="outlined" />
+        <Button>Adicionar Produto</Button>
+      </AddProductContainer>
       <TableContainer>
-        <TableHead>
-          <TableCell>Código</TableCell>
-          <TableCell>Nome</TableCell>
-          <TableCell>Descrição</TableCell>
-          <TableCell>Categorias</TableCell>
-          <TableCell>Quantidade</TableCell>
-          <TableCell>Preço</TableCell>
-        </TableHead>
-        <TableBody />
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Código</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>Descrição</TableCell>
+              <TableCell>Categorias</TableCell>
+              <TableCell>Quantidade</TableCell>
+              <TableCell>Preço</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody />
+        </Table>
       </TableContainer>
     </Container>
   );
