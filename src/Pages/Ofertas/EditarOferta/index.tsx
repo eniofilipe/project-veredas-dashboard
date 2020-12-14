5c01ef6dea27e0f37960b98b6e40e1a508bebb0b
Deise Santana <60986602+deisesan@users.noreply.github.com>
2020-12-14 15:05:05 -0300

    Falta integrar os botões

--- src/Pages/Ofertas/EditarOferta/index.tsx
+++ src/Pages/Ofertas/EditarOferta/index.tsx
@@ -12,7 +12,7 @@ import {
   Paper,
 } from '@material-ui/core';
 import { Container, AddProductContainer } from './styles';
-import { moneyMask } from '../../../Utilities/masks';
+import { viewMoney } from '../../../Utilities/masks';
 
 import { Oferta } from '../../../Types';
 
@@ -30,6 +30,14 @@ const index = () => {
       // eslint-disable-next-line no-console
       console.log(error);
     }
+  };
+
+  const changeOferta = (value: number, pos: number) => {
+    const prodAux = ofertas;
+
+    prodAux[pos] = { ...ofertas[pos], quantidade: value };
+
+    setOfertas([...prodAux]);
   };
 
   useEffect(() => {
@@ -57,20 +65,25 @@ const index = () => {
               <TableCell>Descrição</TableCell>
               <TableCell>Categorias</TableCell>
               <TableCell>Quantidade</TableCell>
-              <TableCell>Preço</TableCell>
+              <TableCell>Valor Unitário</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {ofertas.map((item): JSX.Element => (
+            {ofertas.map((item, pos) => (
               <TableRow hover tabIndex={-1} key={`cod${item.id}`}>
                 <TableCell>{item.produtos.id}</TableCell>
                 <TableCell>{item.produtos.nome}</TableCell>
                 <TableCell>{item.produtos.descricao}</TableCell>
                 <TableCell>{item.produtos.categorias.map((category) => `${category.nome}, `)}</TableCell>
                 <TableCell>
-                  <TextField id="standard-number" type="number" value="item.produtos.descricao" />
+                  <TextField 
+                    id="standard-number" 
+                    type="number" 
+                    value={item.quantidade} 
+                    onChange={(e) => changeOferta(Number(e.target.value), pos)}
+                  />
                 </TableCell>
-                {/* <TableCell>{moneyMask(item.valor_unitario * item.quantidade)}</TableCell> */}
+                <TableCell>{viewMoney(item.valor_unitario)}</TableCell>
               </TableRow>
             ))}
           </TableBody>
